a6d83930777309dac434738bd3a1e3c98da4942e
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-12-08 00:20:11 -0800

    updated models for toJSON and id, and finished routes

--- models/Thought.js
+++ models/Thought.js
@@ -1,44 +1,54 @@
 const { Schema, model } = require('mongoose');
 
-const thoughtSchema = new Schema({
-  thoughText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => new Date(createdAtVal).toISOString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema]
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => new Date(createdAtVal).toISOString(),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  reactions: [reactionSchema]
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
-const reactionSchema = new Schema({
-  reactionId: {
-    type: Schema.Types.ObjectId,
-    default: () => new Types.ObjectId(),
-  },
-  reactionBody: {
-    type: String,
-    required: true,
-    maxLength: 280,
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => new Date(createdAtVal).toISOString(),
-  },
-});
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => new Date(createdAtVal).toISOString(),
+    },
+  }
+);
 
 thoughtSchema.virtual('reactionCount').get(function () {
   return this.reactions.length;
