d773a5b793d286802ab4cfd6eaaf6f6ffc7aec3e
Raymond Bautista <ray@Raymonds-MacBook-Pro.local>
2023-12-10 18:32:41 -0800

    added remaining CRUD operators

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,5 +29,35 @@ module.exports = {
     } catch (error) {
      res.status(500).json(error);
     }
-  }
+  },
+  async updateUser (req,res) {
+    try {
+      const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'No user with that ID' });
+    }
+
+    user.username = req.body.username;
+    user.email = req.body.email;
+
+    await user.save();
+    res.json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async deleteUser (req, res){
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json({ message: 'User successfully deleted', deletedUser });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
 }
